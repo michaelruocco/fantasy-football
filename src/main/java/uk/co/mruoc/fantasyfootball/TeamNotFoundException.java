fbcb5d6c978a6c40a86fb7a9638e41e80cc43d70
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-07-30 20:20:16 +0100

    start again

--- src/main/java/uk/co/mruoc/fantasyfootball/TeamNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package uk.co.mruoc.fantasyfootball;
-
-public class TeamNotFoundException extends FantasyFootballException {
-
-    public TeamNotFoundException(String name) {
-        super("team not found with name " + name);
-    }
-
-}
