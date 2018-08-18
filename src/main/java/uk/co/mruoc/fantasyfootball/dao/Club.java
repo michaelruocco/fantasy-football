f479f7b0b87fbac61c018d4295e90a6fe998216b
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-08-18 21:26:18 +0100

    tidy up redundant code

--- src/main/java/uk/co/mruoc/fantasyfootball/dao/Club.java
+++ src/main/java/uk/co/mruoc/fantasyfootball/dao/Club.java
@@ -1,12 +1,9 @@
 package uk.co.mruoc.fantasyfootball.dao;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.util.Set;
 
 @Entity(name = "club")
 public class Club {
@@ -15,9 +12,6 @@ public class Club {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @OneToMany(mappedBy = "club")
-    private Set<Player> players;
 
     public Club() {
         // required by spring data
@@ -46,14 +40,6 @@ public class Club {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Set<Player> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(Set<Player> players) {
-        this.players = players;
     }
 
 }
