d4bfcb4e6f3e8d1454568b92af2be28fa1d037fb
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-08-26 17:35:09 +0100

    adds component annotation to allow spring to wire up converter

--- src/main/java/uk/co/mruoc/fantasyfootball/web/ClubConverter.java
+++ src/main/java/uk/co/mruoc/fantasyfootball/web/ClubConverter.java
@@ -1,9 +1,11 @@
 package uk.co.mruoc.fantasyfootball.web;
 
+import org.springframework.stereotype.Component;
 import uk.co.mruoc.fantasyfootball.api.ClubDocument;
 import uk.co.mruoc.fantasyfootball.api.ClubDocument.ClubDocumentBuilder;
 import uk.co.mruoc.fantasyfootball.dao.Club;
 
+@Component
 public class ClubConverter {
 
     public ClubDocument toDocument(Club club) {
