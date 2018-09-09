1f0bdf369d34d2217f53ab26e6ae13c138c5cc17
Michael Ruocco <michael.ruocco@hotmail.com>
2018-09-09 09:02:49 +0100

    adds additional coverage to link builders

--- app/src/test/java/uk/co/mruoc/fantasyfootball/app/web/ClubLinkBuilderTest.java
+++ app/src/test/java/uk/co/mruoc/fantasyfootball/app/web/ClubLinkBuilderTest.java
@@ -10,22 +10,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ClubLinkBuilderTest {
 
     private static final long CLUB_ID = 2222;
+    private static final int PAGE_NUMBER = 2;
+    private static final int PAGE_SIZE = 5;
 
     @Test
-    public void shouldReturnEmptyStringIfCurrentRequestCannotBeFound() {
+    public void shouldReturnClubEndpointWithNoHostnameIfCurrentRequestNotSet() {
         final String link = ClubLinkBuilder.build(CLUB_ID);
 
         assertThat(link).isEqualTo("/clubs/2222");
     }
 
     @Test
-    public void shouldReturnLinkIfCurrentRequestIsSetup() {
+    public void shouldReturnClubEndpointLink() {
         try {
             RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
 
             final String link = ClubLinkBuilder.build(CLUB_ID);
 
             assertThat(link).isEqualTo("http://localhost/clubs/2222");
+        } finally {
+            RequestContextHolder.resetRequestAttributes();
+        }
+    }
+
+    @Test
+    public void shouldReturnClubsPaginationEndpointWithNoHostnameIfCurrentRequestNotSet() {
+        final String link = ClubLinkBuilder.build(PAGE_NUMBER, PAGE_SIZE);
+
+        assertThat(link).isEqualTo("/clubs?pageNumber=2&pageSize=5");
+    }
+
+    @Test
+    public void shouldReturnClubsPaginationEndpointLink() {
+        try {
+            RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
+
+            final String link = ClubLinkBuilder.build(PAGE_NUMBER, PAGE_SIZE);
+
+            assertThat(link).isEqualTo("http://localhost/clubs?pageNumber=2&pageSize=5");
         } finally {
             RequestContextHolder.resetRequestAttributes();
         }
--- app/src/test/java/uk/co/mruoc/fantasyfootball/app/web/PlayerLinkBuilderTest.java
+++ app/src/test/java/uk/co/mruoc/fantasyfootball/app/web/PlayerLinkBuilderTest.java
@@ -7,27 +7,47 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ClubPlayersLinkBuilderTest {
+public class PlayerLinkBuilderTest {
 
     private static final long CLUB_ID = 2222;
     private static final int PAGE_NUMBER = 0;
     private static final int PAGE_SIZE = 10;
 
     @Test
-    public void shouldReturnEmptyStringIfCurrentRequestCannotBeFound() {
+    public void shouldReturnClubPlayersPaginatedEndpointWithNoHostnameIfCurrentRequestNotSet() {
         final String link = PlayersLinkBuilder.build(CLUB_ID, PAGE_NUMBER, PAGE_SIZE);
 
         assertThat(link).isEqualTo("/clubs/2222/players?pageNumber=0&pageSize=10");
     }
 
     @Test
-    public void shouldReturnLinkIfCurrentRequestIsSetup() {
+    public void shouldReturnClubPlayersPaginatedEndpoint() {
         try {
             RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
 
             final String link = PlayersLinkBuilder.build(CLUB_ID, PAGE_NUMBER, PAGE_SIZE);
 
             assertThat(link).isEqualTo("http://localhost/clubs/2222/players?pageNumber=0&pageSize=10");
+        } finally {
+            RequestContextHolder.resetRequestAttributes();
+        }
+    }
+
+    @Test
+    public void shouldReturnAllPlayersPaginatedEndpointWithNoHostnameIfCurrentRequestNotSet() {
+        final String link = PlayersLinkBuilder.build(PAGE_NUMBER, PAGE_SIZE);
+
+        assertThat(link).isEqualTo("/players?pageNumber=0&pageSize=10");
+    }
+
+    @Test
+    public void shouldReturnAllPlayersPaginatedEndpoint() {
+        try {
+            RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
+
+            final String link = PlayersLinkBuilder.build(PAGE_NUMBER, PAGE_SIZE);
+
+            assertThat(link).isEqualTo("http://localhost/players?pageNumber=0&pageSize=10");
         } finally {
             RequestContextHolder.resetRequestAttributes();
         }
