d108e30bf60a7d2cdd878d62a706917184f4ed2a
Michael Ruocco <michael.ruocco@hotmail.com>
2018-12-10 20:57:25 +0000

    adds user controller

--- app/src/main/java/uk/co/mruoc/fantasyfootball/app/web/UserController.java
+++ app/src/main/java/uk/co/mruoc/fantasyfootball/app/web/UserController.java
@@ -1,7 +1,6 @@
 package uk.co.mruoc.fantasyfootball.app.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,37 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import uk.co.mruoc.fantasyfootball.api.ArrayDocument;
-import uk.co.mruoc.fantasyfootball.api.PlayerDocument.PlayerData;
-import uk.co.mruoc.fantasyfootball.api.PlayerDocument;
-import uk.co.mruoc.fantasyfootball.api.PlayersDocument;
-import uk.co.mruoc.fantasyfootball.app.dao.Player;
-import uk.co.mruoc.fantasyfootball.app.service.PlayerService;
+import uk.co.mruoc.fantasyfootball.api.UserDocument;
+import uk.co.mruoc.fantasyfootball.app.dao.User;
+import uk.co.mruoc.fantasyfootball.app.service.UserService;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(path= "/players")
-public class PlayerController {
+@RequestMapping(path= "/users")
+public class UserController {
 
-    private static final String DEFAULT_PAGE_SIZE_STRING = "10";
-
-    private final PlayerService service;
-    private final PlayerConverter converter;
-    private final ResponseBuilder<PlayerDocument> responseBuilder = new ResponseBuilder<>();
+    private final UserService service;
+    private final UserConverter converter;
+    private final ResponseBuilder<UserDocument> responseBuilder = new ResponseBuilder<>();
 
     @Autowired
-    public PlayerController(PlayerService service, PlayerConverter converter) {
+    public UserController(UserService service, UserConverter converter) {
         this.service = service;
         this.converter = converter;
     }
 
     @PostMapping
-    public @ResponseBody ResponseEntity<PlayerDocument> create(@Valid @RequestBody final PlayerDocument document) {
-        final boolean exists = document.hasId() && service.exists(document.getId());
-        final Player player = converter.toPlayer(document);
-        final Player createdPlayer = service.create(player);
-        PlayerDocument createdDocument = converter.toDocument(createdPlayer);
+    public @ResponseBody ResponseEntity<UserDocument> create(@Valid @RequestBody final UserDocument document) {
+        final boolean exists = document.hasEmail() && service.existsByEmail(document.getEmail());
+        final User user = converter.toUser(document);
+        final User createdUser = service.create(user);
+        UserDocument createdDocument = converter.toDocument(createdUser);
         if (exists) {
             return responseBuilder.buildUpdatedResponse(createdDocument);
         }
@@ -50,27 +44,19 @@ public class PlayerController {
     }
 
     @PutMapping("/{id}")
-    public @ResponseBody ResponseEntity<PlayerDocument> update(
-            @Valid @PathVariable("id") long id,
-            @RequestBody final PlayerDocument document) {
-        final Player player = converter.toPlayer(id, document);
-        final Player updatedPlayer = service.update(player);
-        PlayerDocument updatedDocument = converter.toDocument(updatedPlayer);
+    public @ResponseBody ResponseEntity<UserDocument> update(
+            @Valid @PathVariable("id") final long id,
+            @RequestBody final UserDocument document) {
+        final User user = converter.toUser(id, document);
+        final User updatedUser = service.update(user);
+        UserDocument updatedDocument = converter.toDocument(updatedUser);
         return responseBuilder.buildUpdatedResponse(updatedDocument);
     }
 
-    @GetMapping("/{id}")
-    public @ResponseBody PlayerDocument read(@PathVariable("id") long id) {
-        final Player player = service.read(id);
-        return converter.toDocument(player);
-    }
-
     @GetMapping
-    public @ResponseBody ArrayDocument<PlayerData> read(
-            @RequestParam(name = "pageNumber", defaultValue = "0") int pageNumber,
-            @RequestParam(name = "pageSize", defaultValue = DEFAULT_PAGE_SIZE_STRING) int pageSize) {
-        final Page<Player> players = service.read(pageNumber, pageSize);
-        return converter.toPlayersDocument(players);
+    public @ResponseBody UserDocument read(@RequestParam("email") final String email) {
+        final User user = service.readByEmail(email);
+        return converter.toDocument(user);
     }
 
 }
